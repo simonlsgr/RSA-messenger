eb9af904e9f1f6936b76b49647bd7998d66f7a48
simonlsr <89278229+simonlsr@users.noreply.github.com>
2022-12-06 23:20:15 +0100

    boilerplate code

--- backend_tests/sending_messages/messages.py
+++ backend_tests/sending_messages/messages.py
@@ -1,10 +1,22 @@
-
+import backend_tests.RSA.rsa as rsa
 
 class main:
-    def __init__(self, message, sender, receiver):
+    def __init__(self, message, sender, receiver, key_a):
         self.message = message
         self.sender = sender
         self.receiver = receiver
+        self.key_a = key_a
 
     def send(self):
-        
+        self.key_a
+        return 
+    
+    def encrypt(self):
+        rsa.encrypt(self.message, self.key_a)
+
+if __name__ == "__main__":
+    message = input("Message: ")
+    sender = int(input("Sender: "))
+    receiver = int(input("Receiver: "))
+    key = input("Key: ")
+    main(message, sender, receiver, key).send()
