1f41f49d9f6dbbccd6027bc04a53844f02675ca8
simonlsr <89278229+simonlsr@users.noreply.github.com>
2022-12-06 19:55:26 +0100

    login backend tests

---
+++ backend_tests/addingusers.py
@@ -0,0 +1,29 @@
+import sqlite3
+import hashlib
+
+conn = sqlite3.connect('users.db')
+cur = conn.cursor()
+
+cur.execute(""" 
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    display_name VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+)
+""")
+
+def create_user(username, display_name, password):
+    password1 = hashlib.sha256(password.encode()).hexdigest()
+    
+    try:
+        cur.execute("INSERT INTO users (username, display_name, password) VALUES (?, ?, ?)", (username, display_name, password1))
+        return "User {} created!".format(username)
+    except sqlite3.IntegrityError:
+        return "Username already exists!"
+
+
+print(create_user("root", "root", "root"))
+print(create_user("Paul", "Paul", "Paul1234"))
+
+conn.commit()
