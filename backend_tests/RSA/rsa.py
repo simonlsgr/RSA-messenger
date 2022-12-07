96322506c6d5815e317882640a3c6d8a3b789619
simonlsr <89278229+simonlsr@users.noreply.github.com>
2022-12-07 15:16:47 +0100

    rsa technically working

--- backend_tests/RSA/rsa.py
+++ backend_tests/RSA/rsa.py
@@ -1,32 +1,60 @@
 import random
 import numpy as np
+import json
 
 class rsa():
     def __init__(self, message):
-        self.message = message
+        try:
+            self.message = int(message)
+        except:
+            self.message = message
     
-    def encrypt(self):
-        pass
+    def encrypt(self, message):
+        key = self.load_key()
+        message_encrypted = []
+        message_buffer = str(self.message)
+        for i in message_buffer:
+            message_encrypted.append(pow(int(i), key["a"], key["n"]))
+        return message_encrypted
+
+    def decrypt(self):
+        key = self.load_key()
+        message_decrypted = []
+        b = self.extended_euclidean_algorithm([key["m"], 1, 0], [key["a"] , 0, 1])
+        for i in self.message:
+            message_decrypted.append(pow(int(i), b, key["n"]))
+        
+        try:
+            return int("".join(map(str, message_decrypted)))
+        except:
+            return message_decrypted
+        
     
     def load_key(self):
+        key = ""
         with open("backend_tests/RSA/key/private_key.json", "r") as f:
             key = f.read()
-        print(key)
+        key = json.loads(key)
+        return key
     
     def generate_key(self):
-        pass
+        dict = self.generate_p_q_n_m(10, 20)
+        dict["a"] = self.generate_coprime(dict["m"])
+        with open("backend_tests/RSA/key/private_key.json", "w") as f:
+            json.dump(dict, f)
 
-    def isPrimeMRT(self, p, k):
+    
+    
+    def is_prime_MRT(self, p, k):
         c = 0
         while k > 0:
-            if self.MillerRabinTest(p) == False:
+            if self.Miller_Rabin_test(p) == False:
                 return False
             k -= 1
             
         return True
     
-
-    def MillerRabinTest(self, p):
+    def Miller_Rabin_test(self, p):
             d = p - 1
             r = 0
 
@@ -50,7 +78,7 @@ class rsa():
 
     
 
-    def generatePrimeNumber(self, length):
+    def generate_prime_number(self, length):
         a = "1"
         z = "9"
         for i in range(length):
@@ -58,41 +86,46 @@ class rsa():
             z += "9"
         while True:
             number = random.randint(int(a), int(z))
-            if self.isPrimeMRT(number, 8):
+            if self.is_prime_MRT(number, 8):
                 return number
 
-    def generatePQNM(self, lowerValue, upperValue):
-        p = self.generatePrimeNumber(random.randint(lowerValue, upperValue))
-        q = self.generatePrimeNumber(random.randint(lowerValue, upperValue))
+    def generate_p_q_n_m(self, lowerValue, upperValue):
+        p = self.generate_prime_number(random.randint(lowerValue, upperValue))
+        q = self.generate_prime_number(random.randint(lowerValue, upperValue))
         n = p * q
         m = (p - 1) * (q - 1)
 
         return {"p": p,"q": q,"n": n,"m": m}
 
-    def EuclideanAlgorithmIsOne(self, a, n):
+    def euclidean_algorithm_is_one(self, a, n):
         if a == 1 and n == 0:
             return True
         elif n == 0:
             return False
-        return self.EuclideanAlgorithmIsOne(int(n), int(int(a) % int(n)))
+        return self.euclidean_algorithm_is_one(int(n), int(int(a) % int(n)))
 
 
-    def generateCoprime(self, n):
+    def generate_coprime(self, n):
         while True:
             a = random.randint(2, n - 1)
-            if self.EuclideanAlgorithmIsOne(a, n) == True:
+            if self.euclidean_algorithm_is_one(a, n) == True:
                 return a
 
-    def extendedEuclideanAlgorithm(self, array1, array2):
+    def extended_euclidean_algorithm(self, array1, array2):
         if array2[0] == 1:
-            return array2[2]
+            return int(array2[2])
         values1 = np.array(array1)
         values2 = np.array(array2)
         factor = values1[0] // values2[0]
         values2 = np.subtract(values1, np.multiply(factor, values2))
-        return self.extendedEuclideanAlgorithm(array2, values2)
+        return self.extended_euclidean_algorithm(array2, values2)
+    
 
     
 if __name__ == "__main__":
-    message = input("Message: ")
-    rsa(message).load_key()
+    message = int(input("Message: "))
+    # rsa(message).generate_key()
+    # rsa(message).load_key()
+    y = rsa(message).encrypt(message)
+    x = rsa(y).decrypt()
+    print(y, x)
