b2a6e1b48d78bfe6c3713237cde3debc3122a62b
simonlsr <89278229+simonlsr@users.noreply.github.com>
2022-12-26 07:50:11 +0100

    organized backend_main.py

--- backend_tests/backend_main.py
+++ backend_tests/backend_main.py
@@ -19,18 +19,28 @@ class main():
         length_bytes_nonce = len(self.nonce)
         encrypted_nonce = self.rsa.encrypt([int.from_bytes(self.nonce, "big")], keyntemp)[0]
 
+        encrypted_ciphertext_byte_length = len(encrypted_message["ciphertext"])
+        tag_byte_length = len(encrypted_message["tag"])
+        
+        encrypted_ciphertext = int.from_bytes(encrypted_message["ciphertext"], "big")
+        tag = int.from_bytes(encrypted_message["tag"], "big")
+        
 
         return {
-            "encrypted_ciphertext": encrypted_message["ciphertext"], 
-            "tag": encrypted_message["tag"] , 
+            "encrypted_ciphertext": encrypted_ciphertext, 
+            "tag": tag,
+            "encrypted_ciphertext_byte_length": encrypted_ciphertext_byte_length,
+            "tag_byte_length": tag_byte_length,
             "encrypted_key": encrypted_key, 
             "encrypted_nonce": encrypted_nonce, 
             "encrypted_key_byte_length": length_bytes_symmetric_key, 
-            "encrypted_nonce_byte_length": length_bytes_nonce}
+            "encrypted_nonce_byte_length": length_bytes_nonce
+        }
     
-    def decrypt(self, encrypted_ciphertext: bytes, tag: bytes, encrypted_key: int, encrypted_nonce: int, encrypted_key_byte_length: int, encrypted_nonce_byte_length: int):
+    def decrypt(self, encrypted_ciphertext: int, tag: int, enrypted_ciphertext_byte_length: int, tag_byte_length, encrypted_key: int, encrypted_nonce: int, encrypted_key_byte_length: int, encrypted_nonce_byte_length: int):
         
-        
+        encrypted_ciphertext = encrypted_ciphertext.to_bytes(enrypted_ciphertext_byte_length, "big")
+        tag = tag.to_bytes(tag_byte_length, "big")
         
         decrypted_key = self.rsa.decrypt([encrypted_key], keyntemp)
         decrypted_nonce = self.rsa.decrypt([encrypted_nonce], keyntemp)
@@ -38,14 +48,20 @@ class main():
 
         decrypted_key = decrypted_key[0].to_bytes(encrypted_key_byte_length, "big")
         decrypted_nonce = decrypted_nonce[0].to_bytes(encrypted_nonce_byte_length, "big")
+
         decrypted_message = self.aes.decrypt(encrypted_ciphertext, tag, decrypted_nonce, decrypted_key)
-        return {"decrypted_message": decrypted_message, "decrypted_key": decrypted_key, "decrypted_nonce": decrypted_nonce, "tag": tag}
+        
+        return decrypted_message.decode("utf-8")
 
 if __name__ == "__main__":
     
-    message = "Test Message"
-    encrypted_out = main().encrypt(message)
-
-    decrypted_out = main().decrypt(encrypted_out["encrypted_ciphertext"], encrypted_out["tag"], encrypted_out["encrypted_key"], encrypted_out["encrypted_nonce"], encrypted_out["encrypted_key_byte_length"], encrypted_out["encrypted_nonce_byte_length"])
+    message = "Test message Öö Ää Üü ß"
+    encrypted_out = main().encrypt(message) 
+    # import json
+    # print(json.dumps(encrypted_out, indent=4))
     
-    print(decrypted_out["decrypted_message"].decode("utf-8"))
+
+
+    decrypted_out = main().decrypt(encrypted_out["encrypted_ciphertext"], encrypted_out["tag"], encrypted_out["encrypted_ciphertext_byte_length"], encrypted_out["tag_byte_length"], encrypted_out["encrypted_key"], encrypted_out["encrypted_nonce"], encrypted_out["encrypted_key_byte_length"], encrypted_out["encrypted_nonce_byte_length"])
+
+    print(decrypted_out)
