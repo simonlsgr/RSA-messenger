5f630a7f644dc69e60fc31bb4a57996237448630
simonlsr <89278229+simonlsr@users.noreply.github.com>
2022-12-27 12:47:57 +0100

    decrypting messages

--- backend_tests/login/TUI_debug.py
+++ backend_tests/login/TUI_debug.py
@@ -25,10 +25,19 @@ if option == "f":
     print(client.recv(1024).decode())
     fetched_messages = client.recv(4294967296).decode()
     list_of_tuples_containing_fetched_messages = eval(fetched_messages)
-    list_encrypted_messages = []
+    list_decrypted_messages = []
+    
     for i, j in enumerate(list_of_tuples_containing_fetched_messages):
-        for k, l in enumerate(j):
-            print(i, l)
+        list_decrypted_messages.append({
+            "sender_id": list_of_tuples_containing_fetched_messages[i][2],
+            "receiver_id": list_of_tuples_containing_fetched_messages[i][1],
+            "message": backend_main.main().decrypt(int(list_of_tuples_containing_fetched_messages[i][3]), int(list_of_tuples_containing_fetched_messages[i][4]), int(list_of_tuples_containing_fetched_messages[i][5]), int(list_of_tuples_containing_fetched_messages[i][6]), int(list_of_tuples_containing_fetched_messages[i][7]), int(list_of_tuples_containing_fetched_messages[i][8]), int(list_of_tuples_containing_fetched_messages[i][9]), int(list_of_tuples_containing_fetched_messages[i][10]), int(list_of_tuples_containing_fetched_messages[i][11]))
+        })
+    
+    for i in list_decrypted_messages:
+        print(i)
+        
+
 
 
 
