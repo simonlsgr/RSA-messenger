48b6a80dae23cf606730f1242badfec5a55e1a08
simonlsr <89278229+simonlsr@users.noreply.github.com>
2023-01-04 12:50:41 +0100

    basic GUI done

--- GUI_client_side/GUI_Flask.py
+++ GUI_client_side/GUI_Flask.py
@@ -5,21 +5,14 @@ import json
 basedir = os.path.abspath(os.path.dirname(__file__))
 datafile = os.path.join(basedir, 'fetched_messages.json')
 
+STANDARD_HEADER = """<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><link rel="icon" href="https://thenounproject.com/api/private/icons/1238399/edit/?backgroundShape=SQUARE&backgroundShapeColor=%23000000&backgroundShapeOpacity=0&exportSize=16&flipX=false&flipY=false&foregroundColor=%23000000&foregroundOpacity=1&imageFormat=png&rotation=0&token=gAAAAABjsyejDaujQ4nYlgT1OD9UIzOlDLBFGj1M_Xm1oraaDDz4BdSK_o2yDS3l0vpmrNCmQV_Z0NKdZulxZhE1D8MkDjN1JA%3D%3D" type="image/x-icon"><link rel="stylesheet" href="static/css/messenger.css">"""
+
 app = flask.Flask(__name__)
 
 @app.route('/index.html')
 def index():
-    html = """<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="https://thenounproject.com/api/private/icons/1238399/edit/?backgroundShape=SQUARE&backgroundShapeColor=%23000000&backgroundShapeOpacity=0&exportSize=16&flipX=false&flipY=false&foregroundColor=%23000000&foregroundOpacity=1&imageFormat=png&rotation=0&token=gAAAAABjsyejDaujQ4nYlgT1OD9UIzOlDLBFGj1M_Xm1oraaDDz4BdSK_o2yDS3l0vpmrNCmQV_Z0NKdZulxZhE1D8MkDjN1JA%3D%3D" type="image/x-icon">
-    <link rel="stylesheet" href="static/css/messenger.css">
-    <title>RSA Messenger</title>
-</head>
-<body>
+    html = STANDARD_HEADER
+    html += """<title>RSA Messenger</title></head><body>
     <main class="main-wrapper">
         <ul class="wrapper-contacts">
     """
@@ -34,16 +27,26 @@ def index():
             html += f"""
             <a href="{j}.html" class="contact" id="contact_id_{j}">{j}</a>
             """
-    html += f"""</ul>
+    html += f"""<button class="add-contacts"></button></ul>
     <section class="section-contact">
             <div class="contact-name-wrapper">
                 <div class="placeholder-contact-name"></div>
                 <h1 class="contact-name">{name}</h1>
             </div>
-            <ul class="texts">
-                 <li class="message sent">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero et autem placeat fugiat beatae adipisci sit reiciendis sed? Sequi ullam assumenda accusantium saepe aperiam velit soluta temporibus, delectus dolorum nihil deleniti dolor laudantium sapiente ut consequatur minus sint! Porro, sit!</li>
-                <li class="message received">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab commodi dolores asperiores qui beatae aut corrupti? Repellendus, quis. Facere natus vitae in molestias ut, aut quidem quaerat veniam modi alias commodi iste nobis itaque accusantium sapiente minima id consequatur. Eaque.</li>
-            </ul>
+            <ul class="texts">"""
+    with open(os.path.join(basedir + "/fetched_messages.json"), "r") as f:
+        data = json.load(f)
+        for i, j in enumerate(data[0]["message"]):
+            if j["sender_name"] == name:
+                html += f"""
+                <li class="message received">{j["message"]}</li>
+                """
+            elif j["receiver_name"] == name:
+                html += f"""
+                <li class="message sent">{j["message"]}</li>
+                """
+
+    html += """</ul>
             <form class="form-message-input">
                 <input type="text" class="message-input" placeholder="Message…">
                 <button class="send-button">↑</button>
@@ -57,14 +60,8 @@ def index():
 
 @app.route('/<name>.html')
 def contact(name):
-    html = f"""<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="https://thenounproject.com/api/private/icons/1238399/edit/?backgroundShape=SQUARE&backgroundShapeColor=%23000000&backgroundShapeOpacity=0&exportSize=16&flipX=false&flipY=false&foregroundColor=%23000000&foregroundOpacity=1&imageFormat=png&rotation=0&token=gAAAAABjsyejDaujQ4nYlgT1OD9UIzOlDLBFGj1M_Xm1oraaDDz4BdSK_o2yDS3l0vpmrNCmQV_Z0NKdZulxZhE1D8MkDjN1JA%3D%3D" type="image/x-icon">
-    <link rel="stylesheet" href="static/css/messenger.css">
+    html = STANDARD_HEADER
+    html += f"""<!DOCTYPE html>
     <title>{name}</title>
 </head>
 <body>
@@ -83,16 +80,28 @@ def contact(name):
                 html += f"""
                 <a href="{j}.html" class="contact" id="contact_id_{j}">{j}</a>
                 """
-    html += f"""</ul>
+    html += f"""<button class="add-contacts"></button></ul>
     <section class="section-contact">
             <div class="contact-name-wrapper">
                 <div class="placeholder-contact-name"></div>
                 <h1 class="contact-name">{name}</h1>
             </div>
-            <ul class="texts">
-                 <li class="message sent">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero et autem placeat fugiat beatae adipisci sit reiciendis sed? Sequi ullam assumenda accusantium saepe aperiam velit soluta temporibus, delectus dolorum nihil deleniti dolor laudantium sapiente ut consequatur minus sint! Porro, sit!</li>
-                <li class="message received">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab commodi dolores asperiores qui beatae aut corrupti? Repellendus, quis. Facere natus vitae in molestias ut, aut quidem quaerat veniam modi alias commodi iste nobis itaque accusantium sapiente minima id consequatur. Eaque.</li>
-            </ul>
+            <ul class="texts">"""
+    
+    with open(os.path.join(basedir + "/fetched_messages.json"), "r") as f:
+        data = json.load(f)
+        for i, j in enumerate(data):
+            if j["sender_name"] == name:
+                for k in j["message"]:
+                    if k["sender_name"] == name:
+                        html += f"""
+                        <li class="message received">{k["message"]}</li>
+                        """
+                    elif k["receiver_name"] == name:
+                        html += f"""
+                        <li class="message sent">{k["message"]}</li>
+                        """
+    html += """</ul>
             <form class="form-message-input">
                 <input type="text" class="message-input" placeholder="Message…">
                 <button class="send-button">↑</button>
